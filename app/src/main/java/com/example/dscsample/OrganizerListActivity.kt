92f75c73dfeee12baa3d66743025fa6f4b43c6a9
Yonit Chauhan <yonit833@gmail.com>
2020-12-12 23:02:42 +0530

    Initial Release

---
+++ app/src/main/java/com/example/dscsample/OrganizerListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dscsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrganizerListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_organizer_list)
+    }
+}
