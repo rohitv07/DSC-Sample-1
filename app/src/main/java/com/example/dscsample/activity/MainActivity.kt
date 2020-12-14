da0ef723e120792c08258eb57df3aa48b9e21d54
Rohit <rv29092000@gmail.com>
2020-12-14 22:30:44 +0530

    Added functionality to the Splash Screen Activity

--- app/src/main/java/com/example/dscsample/activity/MainActivity.kt
+++ app/src/main/java/com/example/dscsample/activity/MainActivity.kt
@@ -1,29 +1,44 @@
 package com.example.dscsample.activity
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dscsample.R
 import com.google.android.material.chip.Chip
 
-lateinit var linkedin_chip:Chip
-lateinit var instagram_chip:Chip
-lateinit var email_chip:Chip
-lateinit var slack_chip:Chip
-lateinit var organizer_btn:Button
-
-
 class MainActivity : AppCompatActivity() {
+
+    lateinit var linkedin_chip:Chip
+    lateinit var instagram_chip:Chip
+    lateinit var email_chip:Chip
+    lateinit var slack_chip:Chip
+    lateinit var organizer_btn:Button
+    lateinit var txtUserName:TextView
+    lateinit var sharedPreferences: SharedPreferences
+
+
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
         setContentView(R.layout.activity_main)
 
-       linkedin_chip = findViewById(R.id.linkedin_chip)
-       instagram_chip = findViewById(R.id.instagram_chip)
-       slack_chip = findViewById(R.id.slack_chip)
-       organizer_btn = findViewById(R.id.organizer_btn)
+        val username= sharedPreferences.getString("Username","User")
+
+        linkedin_chip = findViewById(R.id.linkedin_chip)
+        instagram_chip = findViewById(R.id.instagram_chip)
+        slack_chip = findViewById(R.id.slack_chip)
+        organizer_btn = findViewById(R.id.organizer_btn)
+        txtUserName = findViewById(R.id.txtUserName)
+
+        txtUserName.setText("Welcome ${username},")
 
 
         linkedin_chip.setOnClickListener {
--- app/src/main/java/com/example/dscsample/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/dscsample/activity/SplashScreenActivity.kt
@@ -1,12 +1,70 @@
 package com.example.dscsample.activity
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dscsample.R
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var bottom_layout:LinearLayout
+    lateinit var name_et:EditText
+    lateinit var continue_btn:Button
+    lateinit var sharedPreferences: SharedPreferences
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPreferences = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+
+        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn" , false)
+
         setContentView(R.layout.activity_splash_screen)
+
+        continue_btn = findViewById<Button>(R.id.continue_btn)
+        name_et = findViewById<EditText>(R.id.name_et)
+        bottom_layout = findViewById<LinearLayout>(R.id.bottom_layout)
+
+        bottom_layout.visibility = View.VISIBLE
+
+        continue_btn.setOnClickListener {
+
+            val name = name_et.text.toString()
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("username",name)
+
+            Toast.makeText(this, "Welcome! $name", Toast.LENGTH_SHORT).show()
+
+            savePreferences()
+
+            startActivity(intent)
+        }
+
+        if(isLoggedIn){
+            bottom_layout.visibility = View.GONE
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                val startAct = Intent(this,
+                    MainActivity::class.java)
+                startActivity(startAct)
+                finish()
+            }, 2200)
+        }
+
+    }
+    private fun savePreferences() {
+        sharedPreferences.edit().putBoolean("isLoggedIn", true).apply()
+        sharedPreferences.edit().putString("Username", name_et.text.toString() ).apply()
     }
 }
