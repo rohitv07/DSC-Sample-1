a508f93cae3d1d70b9813bb32e491655ea723063
Yonit Chauhan <yonit833@gmail.com>
2020-12-14 18:17:25 +0530

    Added Welcome Activity

---
+++ app/src/main/java/com/example/dscsample/activity/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dscsample.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dscsample.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
